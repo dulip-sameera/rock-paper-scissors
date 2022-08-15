e1a2b442b05de89f322a8786de6673e6407ab906
DS Weliwaththa <dsgitrepo101@gmail.com>
2022-08-15 14:38:06 +0530

    Add a function to get user input

---
+++ app.js
@@ -0,0 +1,23 @@
+// Choices
+const ROCK = 0;
+const PAPER = 1;
+const SCISSORS = 2;
+
+const choices = ["Rock", "Paper", "Scissors"];
+
+function getPlayerSelection() {
+  do {
+    // Get the player input
+    const selection = window
+      .prompt("Enter Your Choice:(ROCK/PAPER/SCISSORS)")
+      .trim()
+      .toLowerCase();
+
+    // Validates the player input
+    if (selection === "rock") return ROCK;
+    if (selection === "paper") return PAPER;
+    if (selection === "scissors") return SCISSORS;
+  } while (true); // this will loop until one of the correct choices returns
+}
+
+// console.log(getComputerSelection());

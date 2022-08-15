43f81209e9ee2b05baba430f45ccdabbdc03bd06
DS Weliwaththa <dsgitrepo101@gmail.com>
2022-08-15 15:49:01 +0530

    Add function to play a round

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const ROCK = 0;
 const PAPER = 1;
 const SCISSORS = 2;
 
+//round results
+const PLAYER_WIN = 1;
+const PLAYER_LOSE = 0;
+const TIED = -1;
+
 const choices = ["Rock", "Paper", "Scissors"];
 
 function getPlayerSelection() {
@@ -25,4 +30,38 @@ function getComputerSelection() {
   return Math.floor(Math.random() * 3);
 }
 
-// console.log(getComputerSelection());
+function playRound(playerSelection, computerSelection) {
+  // roundResult = [playerSelection, computerSelection, PLAYER_WIN/PLAYER_LOSE/TIED]
+  let roundResult = [];
+
+  roundResult.push(playerSelection);
+  roundResult.push(computerSelection);
+
+  // check for round TIED
+  if (playerSelection === computerSelection) {
+    roundResult.push(TIED);
+    return roundResult;
+  }
+
+  // check whether the player is the winner
+  if (
+    (playerSelection === PAPER && computerSelection === ROCK) ||
+    (playerSelection === ROCK && computerSelection === SCISSORS) ||
+    (playerSelection === SCISSORS && computerSelection === PAPER)
+  ) {
+    roundResult.push(PLAYER_WIN);
+    return roundResult;
+  }
+
+  // check whether the player is the loser
+  if (
+    (computerSelection === PAPER && playerSelection === ROCK) ||
+    (computerSelection === ROCK && playerSelection === SCISSORS) ||
+    (computerSelection === SCISSORS && playerSelection === PAPER)
+  ) {
+    roundResult.push(PLAYER_LOSE);
+    return roundResult;
+  }
+}
+
+console.log(playRound(getPlayerSelection(), getComputerSelection()));
